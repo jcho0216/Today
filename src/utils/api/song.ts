19c92e3ea44070a147129b9d07487fa66c2c665f
Jun Joy <chojuneseo1@gmail.com>
2022-06-16 19:54:20 +0900

    set env for today

--- src/utils/api/song.ts
+++ src/utils/api/song.ts
@@ -2,8 +2,7 @@ import axios from "axios";
 import { Top10SongsType } from "../../interface/Song";
 
 export const getTop10Songs = async () => {
-  console.log(import.meta.env);
-  const data = await axios.get(`${import.meta.env.VITE_WS_BASE_URL}/music/top10`);
+  const data = await axios.get(`${import.meta.env.VITE_VERCEL_WS_BASE_URL}/music/top10`);
 
   return data.data.music as Top10SongsType[];
 };
